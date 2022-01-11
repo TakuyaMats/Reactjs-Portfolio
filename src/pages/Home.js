82478dce8520eb4901f787e1210d069362b68f92
TMats <takomats@hotmail.com>
2022-01-10 22:50:37 -0500

    changed the navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
-import AboutSection from '../components/AboutSection/AboutSection'
+import MenuAppBar from '../components/Navbar/Navbar';
 
 const HomeStyle = styled.div`
-    
+        /* .hero {
+        height: 100vh;
+        min-height: 1000px;
+        width: 100%;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+    } */
 `
 
 function Home() {
     return(
         <HomeStyle>
-            <AboutSection />
+            {/* <div className="hero"> */}
+                <MenuAppBar />
+            {/* </div> */}
         </HomeStyle>
     )
 }

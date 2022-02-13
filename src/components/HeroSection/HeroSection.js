517f33de1fbf3eec17348c13b1044990f5fb2863
Takuya Matsumoto <tmatsnj@gmail.com>
2022-02-13 01:41:08 -0500

    added image to the HeroSection

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,11 +1,30 @@
+import styled from 'styled-components';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
+import heroAvatar from '../../assets/images/heroAvatar.png';
+
+const HeroSectionStyle = styled.div`
+    background-color: #E5E8DD
+`;
 
 const HeroSection = () => {
     return (
-        <Container>
-            <h1>Hello from HeroSection!</h1>
-        </Container>
-    )
+        <HeroSectionStyle>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2} columns={16}>
+                    <Grid item xs={8}>
+                        <Container>Hello</Container>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Container id="hero-container">
+                            <img id="hero-img" src={heroAvatar} alt="Hero Avatar"/>
+                        </Container>
+                    </Grid>
+                </Grid>
+            </Box>
+        </HeroSectionStyle>
+    );
 };
 
 export default HeroSection;

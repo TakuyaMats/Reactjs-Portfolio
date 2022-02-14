c5825b93fc720cb72b9ea80e354eb9e15177a5e4
Takuya Matsumoto <tmatsnj@gmail.com>
2022-02-13 19:54:30 -0500

    got rid of styled component

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,33 +1,25 @@
-import styled from 'styled-components';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import heroAvatar from '../../assets/images/heroAvatar.png';
 
-const HeroSectionStyle = styled.div`
-    background-color: #E5E8DD
-
-`;
-
 const HeroSection = () => {
     return (
-        <HeroSectionStyle>
-            <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={2} columns={16}>
-                    <Grid item xs={8}>
-                        <Container>
-                            <h1 style={{ margin: 24 }}>Hi, My Name Is Takuya Matsumoto</h1>
-                            <p style={{  display: 'flex', justifyContent: 'flex-end', fontSize: 18, color: 'grey' }}>I love all things music, tech, and design!</p>
-                        </Container>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Container id="hero-container">
-                            <img id="hero-img" src={heroAvatar} alt="Hero Avatar" style={{ height: '100%', width: '100%', align: "center"}}/>
-                        </Container>
-                    </Grid>
+        <Box sx={{ flexGrow: 1, backgroundColor: '#E5E8DD'}}>
+            <Grid container spacing={2} columns={16}>
+                <Grid item xs={8}>
+                    <Container>
+                        <h1 style={{ margin: 24 }}>Hi, My Name Is Takuya Matsumoto</h1>
+                        <p style={{  display: 'flex', justifyContent: 'flex-end', fontSize: 18, color: 'grey' }}>I love all things music, tech, and design!</p>
+                    </Container>
                 </Grid>
-            </Box>
-        </HeroSectionStyle>
+                <Grid item xs={8}>
+                    <Container id="hero-container">
+                        <img id="hero-img" src={heroAvatar} alt="Hero Avatar" style={{ height: '100%', width: '100%', align: "center"}}/>
+                    </Container>
+                </Grid>
+            </Grid>
+        </Box>
     );
 };
 
